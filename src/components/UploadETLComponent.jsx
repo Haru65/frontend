5e485e10aada041a943b33fb759801c58e7f253c
unknown <adityakhandagale69@gmail.com>
2025-07-24 03:26:10 +0530

    last commit

--- src/components/UploadETLComponent.jsx
+++ src/components/UploadETLComponent.jsx
@@ -112,7 +112,7 @@ export default function UploadETLComponent({ onETLComplete }) {
 
       setUploadStatus('processing');
 
-      const response = await fetch('https://backend-m6sp.onrender.com/run_etl/', {
+      const response = await fetch('https://backend-1-n2xg.onrender.com/run_etl/', {
         method: 'POST',
         body: formData,
       });
